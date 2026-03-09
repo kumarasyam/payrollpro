934ee810cad0057f464f56eff174a7e17ecbaca5
Kumar <kumar@payrollpro.com>
2026-03-10 01:24:10 +0530

    Fix: Correct module resolution for db.cjs

--- server/index.cjs
+++ server/index.cjs
@@ -4,7 +4,7 @@
  */
 const express = require('express');
 const cors = require('cors');
-const { sql, getPool } = require('./db');
+const { sql, getPool } = require('./db.cjs');
 require('dotenv').config();
 
 const app = express();
